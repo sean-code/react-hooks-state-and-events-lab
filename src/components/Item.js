21a0a8159d529daaaaba1a72755125ac9b66d328
sean-code <john.nganga@student.moringaschool.com>
2022-10-05 15:21:12 +0300

    Re-ctifications done and all tests passing

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const[isInCart,setIsInCart]=useState(false)
+  function handleCart(e){
+    setIsInCart(isInCart => !isInCart)
+    console.log(e)
+  }
   return (
-    <li className="">
+    <li className= {isInCart? "in-cart":""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{isInCart? "Remove From Cart":"Add to Cart" }</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,33 @@
-import React from "react";
+
+
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  let [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  let displayItems = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">All Categories</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
